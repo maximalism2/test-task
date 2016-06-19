96601bffdf457af7e69af6aef83a58a7512f2b4e
Max Vyznyuk <mvyznyuk@gmail.com>
2016-06-19 03:43:55 +0300

    Add icon to search input, add global loading spinner, other small improvements

--- src/app/components/list.js
+++ src/app/components/list.js
@@ -234,8 +234,33 @@ class ListOfClients extends Component {
             placeholder="search"
             className="client-search"
             onChange={e => this.props.search(e.target.value)}
+            onKeyDown={e => this.keydownHandler(e)}
             value={this.props.list.data.searchQuery}
           />
+          {this.props.list.data.searchQuery.length ?
+            <i
+              className="input-icon"
+              onClick={() => this.props.resetSearch()}
+            >⨉</i>
+          : 
+            <i
+              className="input-icon simple"
+            >
+              <svg
+                className="search-icon"
+                x="0px"
+                y="0px"
+                viewBox="0 0 52.966 52.966"
+                width="17px"
+                height="17px"
+              >
+                <path
+                  d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21  c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279  C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19  S32.459,40,21.983,40z"
+                  fill="#aaaaaa"
+                />
+              </svg>
+            </i>
+          }
         </div>
         <div className="list-of-clients">
           {this.renderList()}
--- src/app/container/index.js
+++ src/app/container/index.js
@@ -2,20 +2,41 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { ClientData, ListOfClients } from '../components/';
-
 import * as actionsData from '../actions/clientData';
 import * as actionsList from '../actions/list';
+import cnames from 'classnames';
 
 class MainContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showed: false
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        showed: true
+      });
+    }, 50);
+  }
+
   render() {
     let { clientData, list, dispatch } = this.props;
 
     const boundDataActions = bindActionCreators(actionsData, dispatch);
     const boundListActions = bindActionCreators(actionsList, dispatch);
 
+    let cardCNames = cnames({
+      "card": 1,
+      "fade-out": !this.state.showed
+    });
+
     return (
       <div className="wrapper">
-        <div className="card">
+        <div className={cardCNames}>
           <div className="clients-list">
             <ListOfClients
               {...boundListActions}
